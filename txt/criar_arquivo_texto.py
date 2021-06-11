a5ddfdbe76cec38c8e12a1d9b42e65a3800e13c1
Weverton Teixeira <weverton.tsousa@hotmail.com>
2021-06-11 15:07:23 -0300

    Alterando txt

--- txt/criar_arquivo_texto.py
+++
@@ -1,7 +0,0 @@
-with open('txt/arquivo_criado.txt', 'w', encoding='utf8') as f:
-    f.writelines(['Arroz\n', 'Feijão\n', 'Café\n'])
-
-    f.write('Miojo\n')
-    f.write('Requeijão\n')
-
-    print('Arquivo criado com sucesso.')
