8d628148b081bf2f7262ad51874d30fd9f44fa20
Weverton Teixeira <weverton.tsousa@hotmail.com>
2021-06-11 21:36:59 -0300

    Alterado Excel

--- excel/criar_arquivo_excel.py
+++ excel/criar_arquivo_excel.py
@@ -3,13 +3,17 @@ from openpyxl import Workbook
 arquivo_excel = Workbook()
 
 planilha_1 = arquivo_excel.active
+# -----------------------------------------------------------------------------
 
-# Por padrão é criado planilha1, e você pode alterar o nome
+''' Por padrão é criado planilha1, e você pode alterar o nome '''
+
 planilha_1.title = 'Produtos'
+# -----------------------------------------------------------------------------
 
 planilha_1['A1'] = 'Nome'
 planilha_1['B1'] = 'Qtde'
 planilha_1['C1'] = 'Preço'
+# -----------------------------------------------------------------------------
 
 produtos = [
     ('Arroz', 1, 28.30),
@@ -19,20 +23,29 @@ produtos = [
 
 for produto in produtos:
     planilha_1.append(produto)
+# -----------------------------------------------------------------------------
 
 planilha_1.cell(row=5, column=1, value='Requeijão')
 planilha_1.cell(row=5, column=2, value=1)
 planilha_1.cell(row=5, column=3, value=5.40)
+# -----------------------------------------------------------------------------
 
 planilha_1['C6'] = '=SOMA(C2:C5)'
-
+# -----------------------------------------------------------------------------
 
 a2 = planilha_1['A2']
-print(a2.value)  # Arroz
+print(a2.value)
+
+# Resultado:
+# Arroz
+# -----------------------------------------------------------------------------
 
 a3 = planilha_1.cell(column=1, row=3)
-print(a3.value)  # Feijão
+print(a3.value)
 
+# Resultado:
+# Feijão
+# -----------------------------------------------------------------------------
 
 total_de_linhas_usadas = planilha_1.max_row
 total_de_colunas_usadas = planilha_1.max_column
@@ -41,6 +54,18 @@ for linha in range(1, total_de_linhas_usadas, 1):
     for coluna in range(1, total_de_colunas_usadas, 1):
         print(planilha_1.cell(row=linha, column=coluna).value)
 
-arquivo_excel.save('excel/arquivo_criado.xlsx')
+# Resultado:
+# Nome
+# Qtde
+# Arroz
+# 1
+# Feijão
+# 1
+# Café
+# 1
+# Requeijão
+# 1
+# -----------------------------------------------------------------------------
 
+arquivo_excel.save('excel/arquivo_criado.xlsx')
 arquivo_excel.close()
