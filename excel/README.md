0ff6b356b01ccd7f8daf5187908b4a403ec1258d
Weverton Teixeira <weverton.tsousa@hotmail.com>
2021-06-26 18:58:52 -0300

    Del

--- excel/README.md
+++
@@ -1,4 +0,0 @@
-Quando criamos uma classe Worlbook(), estamos criando uma área de trabalho
-na memória.
-
-O arquivo Excel somente é criado quando usamos a função save()
--- excel/ler_arquivo_excel.py
+++
@@ -1,9 +0,0 @@
-from openpyxl import load_workbook
-
-arquivo_excel = load_workbook('excel/arquivo_criado.xlsx')
-
-arquivo_excel.create_sheet('Estados')
-
-print(arquivo_excel.sheetnames)
-
-arquivo_excel.close()
